b554f87379d2c1f94979653656af966ef2c838e8
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2016-12-17 17:51:50 +0100

    zmiana na assertThat w BinarySearchJestOstatnimElementem

--- src/test/java/lab2_1/BinarySearchJestOstatnimElementem.java
+++ src/test/java/lab2_1/BinarySearchJestOstatnimElementem.java
@@ -8,20 +8,23 @@ import org.junit.Test;
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 public class BinarySearchJestOstatnimElementem {
 
 	@Test
-public void test(){
+	public void test(){
 	int key = 3;
 	int[] seq = {1,2,3};
 	int i;
+	boolean wart=true;
 	
 	SearchResult searchResult = BinarySearch.search(key, seq);
 	i=searchResult.getPosition();
 	
-	assertTrue(searchResult.isFound());
-	assertEquals(key, seq[i]);
+	
+	assertThat(wart,is(equalTo(searchResult.isFound())));
+	assertThat(key,is(equalTo(seq[i])));
 	
 
 }
