512b629639307970f6b95f5c7d12de96febd9eef
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2016-12-17 16:32:17 +0100

    Test BinarySearchJestPierwszymElementem 2

--- src/test/java/lab2_1/BinarySearchJestPierwszymElementem.java
+++ src/test/java/lab2_1/BinarySearchJestPierwszymElementem.java
@@ -1,8 +1,18 @@
 package lab2_1;
 
+import org.junit.Test;
+
 public class BinarySearchJestPierwszymElementem {
 	
-	
-	
+	@Test
+	public void test(){
+		
+		int key=1;
+		int[] seq={1,2,3};
+		int i;
+		
+		
+		
+	}
 
 }
