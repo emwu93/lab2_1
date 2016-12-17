384668be4c4c0ab85d64ac1f904424a95c119d76
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2016-12-17 16:26:51 +0100

    Test BinarySearchNieJestWSekwecji 2

--- src/test/java/lab2_1/BinarySearchNieJestWSekwencji.java
+++ src/test/java/lab2_1/BinarySearchNieJestWSekwencji.java
@@ -1,5 +1,26 @@
 package lab2_1;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+
 public class BinarySearchNieJestWSekwencji {
+	
+	@Test
+	void test(){
+		
+		int key=1;
+		int[] seq={2};
+		int i;
+		
+		SearchResult searchResult = BinarySearch.search(key, seq);
+		i=searchResult.getPosition();
+		
+		assertFalse(searchResult.isFound());
+		assertEquals(-1,i);
+	}
 
 }
