44c78feef6585b0ba6d2b500472a94f211eb9e8d
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2016-12-17 15:49:21 +0100

    Test BinarySearchJestWSekwencji 1

--- src/test/java/lab2_1/BinarySearchJestWSekwencjiTest.java
+++ src/test/java/lab2_1/BinarySearchJestWSekwencjiTest.java
@@ -1,5 +1,24 @@
 package lab2_1;
 
-public class BinarySearchJestWSekwencjiTest {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+
+public class BinarySearchJestWSekwencjiTest {
+	
+	
+	
+	@Test
+	public void test(){
+		
+		int key=1;
+		int[] seq ={1,2};
+		
+		assertSame(1,BinarySearch.search(key, seq));
+		
+	}
+	
+	
 }
