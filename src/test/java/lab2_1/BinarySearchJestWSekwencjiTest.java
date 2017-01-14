4f04493600dae1fc7e945065361069f22aa37b23
Mateusz Wierzbicki <mwierzbicki93@gmail.com>
2017-01-14 14:46:40 +0100

    Refaktoryzacja testu BSJestWSekwencji

--- src/test/java/lab2_1/BinarySearchJestWSekwencjiTest.java
+++ src/test/java/lab2_1/BinarySearchJestWSekwencjiTest.java
@@ -1,6 +1,8 @@
 package lab2_1;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
@@ -14,11 +16,13 @@ public class BinarySearchJestWSekwencjiTest {
 		int key = 1;
 		int[] seq = {1};
 		int i;
+		boolean wart=true;		
 		
 		SearchResult searchResult = BinarySearch.search(key, seq);
 		i=searchResult.getPosition();
 		
-		assertTrue(searchResult.isFound());
+		assertThat(wart,is(equalTo(searchResult.isFound())));
+		//assertTrue(searchResult.isFound());
 		assertEquals(key, seq[i]);
 	}
 	
